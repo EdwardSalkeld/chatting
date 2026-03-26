3fd6cb251d1e7561c0e707a0284c0c76c4482c63
billy <billy@alcachofa.faith>
2026-03-26 17:10:33 +0000

    Fix several permissions issues since dropping root

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ if [ "$PUID" != "0" ] || [ "$PGID" != "0" ]; then
         useradd -u "$PUID" -g "$PGID" -d "$HOME" -s /bin/sh "chatting_$PUID"
     fi
     chown -R "$PUID:$PGID" "$HOME"
+    # Runtime state lives on mounted volumes; ensure they remain writable after dropping privileges.
+    for path in /data /tmp; do
+        if [ -e "$path" ]; then
+            chown -R "$PUID:$PGID" "$path"
+        fi
+    done
     exec gosu "$PUID:$PGID" "$@"
 fi
 
